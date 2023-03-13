ffe99565d558675a40b1d69b548b9b2d9dd4efd4
Bruno Macena <macena@outlook.com>
2023-03-13 19:15:40 -0300

    Menu Selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,12 @@ $('.carousel-control-prev').on('click', function () {
 $(document).on('click', function() {
     return $('.collapse').collapse('hide');
 });
+
+// Menu selection
+
+$(document).ready(function() {
+    $("ul.navbar-nav > li > a").click(function(e) {
+      $("ul.navbar-nav > li > a").removeClass("active");
+      $(this).addClass("active");
+    });
+  });
