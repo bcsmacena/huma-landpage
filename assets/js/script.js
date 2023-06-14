44dc5ff5d89a60d5280974f7464c4d2cd32dd37a
Fmacena <fmacenalytics@outlook.com>
2023-06-14 23:54:39 +0100

    header>navbar>exibe nome da section em telas mobile (que mostram o burger menu)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,19 @@ $(document).ready(function() {
     $("ul.navbar-nav > li > a").click(function(e) {
       $("ul.navbar-nav > li > a").removeClass("active");
       $(this).addClass("active");
+      var menuItem = $(this).text();  
+      if (menuItem == "Home") {
+        $('#navbarText').hide();
+      } else {       
+        $('#navbarText').show();
+        document.getElementById("navbarText").innerHTML = menuItem;
+      }
+    });
+  });
+
+  $(document).ready(function(){
+    $('.navbar-brand').click(function(){
+      // document.getElementById("navbarText").innerHTML = "";
+      $('#navbarText').hide();
     });
   });
